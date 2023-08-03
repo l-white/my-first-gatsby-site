ed0a479ccae27fd44330a9ff448cb046b14ab5c5
Laura <law7676@outlook.com>
2023-08-03 13:01:28 -0500

    added blog posts page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 // Step 2: Define your component
 const AboutPage = () => {
@@ -18,7 +19,7 @@ const AboutPage = () => {
 
 export const Head = () => (
   <>
-    <title>About Me</title>
+    <Seo title="About Me" />
     <meta name="description" content="About me" />
   </>
 )
